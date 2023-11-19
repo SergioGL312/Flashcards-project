11fc43e10aebd80f0cc90cfb0f2af9219dc7ee1e
Sergio GL <sergiogonzalezlopez101@gmail.com>
2023-11-19 17:20:02 -0600

    Added update and delete in Flashcards

--- src/Components/FlashcardCard.js
+++ src/Components/FlashcardCard.js
@@ -1,37 +1,132 @@
+import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { TouchableOpacity, Text, View } from 'react-native';
+import { TouchableOpacity, Text, View, TextInput } from 'react-native';
+import { Overlay } from '@rneui/base';
+import { Icon, Button } from '@rneui/themed';
 
 // CONSTANTS
 import { ROUTES } from '../Constants/navigation.constants';
 // HOOKS
 import { useCards } from '../hooks/data';
+import { useModal } from '../hooks/modal';
 // UTILS
 import { pluralize } from '../utils/text';
+// FIREBASE
+import { FLASHCARDS } from '../api/db';
 
-export default function FlascardCard({ flashcard }) {
+export default function FlascardCard({ flashcard, isLongPressed, onLongPress }) {
   const navigation = useNavigation();
   const { nameFlashcard, id } = flashcard;
   const cards = useCards(id);
+  const { visible, show, hide } = useModal();
+  const [newName, setNewName] = useState(nameFlashcard);
+
+
+  const editFlashcard = async (flashcardId, newName) => {
+    const flashcardRef = FLASHCARDS.doc(flashcardId);
+
+    try {
+      await flashcardRef.update({
+        nameFlashcard: newName,
+      });
+      console.log("Flashcard successfully updated.");
+      hide();
+    } catch (error) {
+      console.error("Error updating flashcard: ", error);
+    }
+  };
+
+  const deleteFlashcard = async (flashcardId) => {
+    const flashcardRef = FLASHCARDS.doc(flashcardId);
+
+    try {
+      await flashcardRef.delete();
+      console.log("Flashcard successfully deleted.");
+    } catch (error) {
+      console.error("Error deleting flashcard: ", error);
+    }
+  };
 
   return (
-    <TouchableOpacity
-      style={{
+    <View style={{ flexDirection: 'row', }}>
+
+      <Overlay
+        overlayStyle={{ backgroundColor: "#4C73F5" }}
+        isVisible={visible}
+        onBackdropPress={hide}
+      >
+        <View>
+          <Text style={{ color: "white" }}>New Name</Text>
+
+          <TextInput
+            style={{ color: "black" }}
+            onChangeText={(text) => setNewName(text)}
+            value={newName}
+          />
+        </View>
+
+        <Button
+          title="Edit"
+          onPress={() => editFlashcard(id, newName)}
+        />
+      </Overlay>
+
+      <View style={{
         padding: 24,
         marginBottom: 24,
         borderRadius: 12,
-      }}
-      onPress={() => navigation.navigate(ROUTES.cards.name, { flashcard })}
-      activeOpacity={0.5}
-    >
-      <Text style={{ color: 'black' }}>{nameFlashcard}</Text>
+        width: '75%'
+      }}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate(ROUTES.cards.name, { flashcard })}
+          onLongPress={onLongPress}
+          activeOpacity={0.5}
+        >
+          <View>
+            <Text style={{ color: 'black' }}>{nameFlashcard}</Text>
 
-      {!!cards.length && (
-        <View>
-          <Text>
-            {pluralize({ noun: 'Card', number: cards.length })}
-          </Text>
+            {!!cards.length && (
+              <View>
+                <Text>
+                  {pluralize({ noun: 'Card', number: cards.length })}
+                </Text>
+              </View>
+            )}
+          </View>
+        </TouchableOpacity>
+      </View>
+
+      {isLongPressed && (
+        <View style={{
+          padding: 24,
+          marginBottom: 24,
+          borderRadius: 12,
+        }}>
+          <Button
+            type='clear'
+            icon={
+              <Icon
+                name="edit"
+                type="material"
+                color="blue"
+              />
+            }
+            onPress={show}
+          />
+
+          <Button
+            type='clear'
+            icon={
+              <Icon
+                name="delete"
+                type="material"
+                color="red"
+              />
+            }
+            onPress={() => deleteFlashcard(id)}
+          />
         </View>
       )}
-    </TouchableOpacity>
+    </View>
   );
 }
--- src/Screens/Flashcards.screen.jsx
+++ src/Screens/Flashcards.screen.jsx
@@ -1,70 +1,75 @@
-import React, {useEffect} from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, TouchableWithoutFeedback } from "react-native";
 import { useCallback } from "react";
+
 // COMPONENTS
 import AddFlashcard from "../Components/AddFlashcard";
 import { useFlashcards } from "../hooks/data";
 import FlascardCard from "../Components/FlashcardCard";
 
-
 //Diseño 
-import {useFonts} from 'expo-font';
+import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
-
 export default function Flashcards() {
-
   const flashcards = useFlashcards();
+  const [longPressedId, setLongPressedId] = useState(null);
 
+  useEffect(() => {
+    if (longPressedId !== null) {
+      console.log(`ID: ${longPressedId}`);
+    }
+  }, [longPressedId]);
 
-      //Things about the LOADFONTS
-      const [fontsLoaded] = useFonts({
-        Bebas: require("../assets/fonts/BebasNeue-Regular.ttf")
-      }); 
-    
-      //SplashScreen for Loading Fonts
-    
-      useEffect (()=> {
-        async function prepare (){
-          await SplashScreen.preventAutoHideAsync();
-        }
-        prepare (); 
-      }, [])
-    
-    
-      const onLayout = useCallback(async() => {
-        if (fontsLoaded){
-          await SplashScreen.hideAsync();
-        
-      }
-      }, [fontsLoaded]);
-  
-      if (!fontsLoaded) return null;
-  
-      //
+  //Things about the LOADFONTS
+  const [fontsLoaded] = useFonts({
+    Bebas: require("../assets/fonts/BebasNeue-Regular.ttf")
+  });
+
+  //SplashScreen for Loading Fonts
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, [])
+
+  const onLayout = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
 
   return (
-    <View>
-      <Text
-        style={styles.texto}
-      >Flashcards</Text>
-      
-      <AddFlashcard />
-
+    <TouchableWithoutFeedback onPress={() => setLongPressedId(null)}>
       <View>
-        {flashcards.length ? (
-          flashcards.map((flashcard, index) => (
-            <FlascardCard
-              key={flashcard.id}
-              flashcard={flashcard}
-            />
-          )
-          )) : (
-          <Text>Try adding a new category</Text>
-        )}
-      </View>
+        <Text
+          style={styles.texto}
+        >Flashcards</Text>
 
-    </View>
+        <AddFlashcard />
+
+        <View>
+          {flashcards.length ? (
+            flashcards.map((flashcard, index) => (
+              <FlascardCard
+                key={flashcard.id}
+                flashcard={flashcard}
+                isLongPressed={longPressedId === flashcard.id}
+                onLongPress={() => setLongPressedId(flashcard.id)}
+              />
+            )
+            )) : (
+            <Text>Try adding a new category</Text>
+          )}
+        </View>
+
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -75,6 +80,6 @@ const styles = StyleSheet.create({
     fontSize: 30,
     textAlign: "center",
     marginTop: "20%"
-  }, 
+  },
 
 })
